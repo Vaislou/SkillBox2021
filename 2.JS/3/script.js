ae035cd5bf5cabdec69a0e1e2ac598032ed4151d
Vaislou <vaislou@gmail.com>
2021-09-12 20:47:52 +0300

    fix: fix issues

--- 2.JS/3/script.js
+++ 2.JS/3/script.js
@@ -21,24 +21,24 @@
 
 //2
 
-function transformUserInfo(name, surname) {
-    let changedName = name.toLowerCase().split('');
-    changedName[0] = changedName[0].toUpperCase();
-    changedName = changedName.join('');
+// function transformUserInfo(name, surname) {
+//     let changedName = name.toLowerCase().split('');
+//     changedName[0] = changedName[0].toUpperCase();
+//     changedName = changedName.join('');
 
-    let changedSurname = surname.toLowerCase().split('');
-    changedSurname[0] = changedSurname[0].toUpperCase();
-    changedSurname = changedSurname.join('');
+//     let changedSurname = surname.toLowerCase().split('');
+//     changedSurname[0] = changedSurname[0].toUpperCase();
+//     changedSurname = changedSurname.join('');
 
-    console.log(`Имя пользователя: ${changedName}`);
-    console.log(`Фамилия пользователя: ${changedSurname}`);
+//     console.log(`Имя пользователя: ${changedName}`);
+//     console.log(`Фамилия пользователя: ${changedSurname}`);
 
-    (changedName === name) ? console.log('Имя осталось без изменений') : console.log('Имя было преобразовано');
-    (changedSurname === surname) ? console.log('Фамилия осталась без изменений') : console.log('Фамилия была преобразована');
-}
+//     (changedName === name) ? console.log('Имя осталось без изменений') : console.log('Имя было преобразовано');
+//     (changedSurname === surname) ? console.log('Фамилия осталась без изменений') : console.log('Фамилия была преобразована');
+// }
 
-transformUserInfo('Иван', 'иванОв');
-transformUserInfo('сергей', 'сергеев');
-transformUserInfo('ПЕТР', 'ПЕТРОВ');
-transformUserInfo('васиЛиЙ', 'Васильев');
-transformUserInfo('Александр', 'Александрович');
+// transformUserInfo('Иван', 'иванОв');
+// transformUserInfo('сергей', 'сергеев');
+// transformUserInfo('ПЕТР', 'ПЕТРОВ');
+// transformUserInfo('васиЛиЙ', 'Васильев');
+// transformUserInfo('Александр', 'Александрович');
